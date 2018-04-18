cdfe3e7726d238ed720ad6516350f77ac78a888e
yuhy <yhy_working@163.com>
2018-04-18 17:11:59 +0800

    java并发编程学习-多线程的四种实现方式

--- src/test/ConcurrentStudy/Mythread.java
+++ src/test/ConcurrentStudy/Mythread.java
@@ -1,7 +1,9 @@
 package ConcurrentStudy;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.FutureTask;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * 创建多线程的四种方式
@@ -34,7 +36,8 @@ public class Mythread {
         }
     }
 
-    static class MyCallable<T> implements Callable<T>{
+    //实现Callable接口通过FutureTask包装器来创建Thread线程
+    class MyCallable<T> implements Callable<T>{
 
         @Override
         public T call() throws Exception {
@@ -43,15 +46,78 @@ public class Mythread {
         }
     }
 
+    //使用ExecutorService、Callable、Future实现有返回结果的线程
+    class MyCallable1 implements Callable<Object> {
+
+        private String taskNum;
+
+        public MyCallable1(String taskNum) {
+            this.taskNum = taskNum;
+        }
+
+        @Override
+        public Object call() throws Exception {
+            System.out.println(">>>"+taskNum+"任务启动");
+            Date dateTmpl = new Date();
+            Thread.sleep(1000);
+            Date dateTmpl2 = new Date();
+            long time = dateTmpl2.getTime() - dateTmpl.getTime();
+            System.out.println(">>>"+taskNum+"任务终止");
+
+            return taskNum + "任务返回运行结果，当前任务时间【"+time+"毫秒";
+        }
+    }
+    class Mythread4 {
+        //线程池数量
+       public void  myRun() throws ExecutionException, InterruptedException {
+           Date date1 = new Date();
+           int taskSize = 5;
+           //创建一个线程池
+           ExecutorService pool = Executors.newFixedThreadPool(taskSize);
+           //创建多个有返回值的任务
+           List<Future> list = new ArrayList<Future>();
+           for (int i = 0; i < taskSize; i++) {
+               Callable c = new Mythread().new MyCallable1(i + " ");
+               //执行任务并获取Future对象
+               Future f = pool.submit(c);
+               list.add(f);
+           }
+           //关闭线程池
+           pool.shutdown();
+           //获取所以并发任务的运行结果
+           for (Future f : list) {
+               // 从Future对象上获取任务的返回值，并输出到控制台
+               System.out.println(">>>" + f.get().toString());
+           }
+
+           Date date2 = new Date();
+           System.out.println("----程序结束运行----，程序运行时间【"
+                   + (date2.getTime() - date1.getTime()) + "毫秒】");
+       }
+    }
+
     public static void main(String[] args) {
+        System.out.println("主线程---开始运行");
         Mythread1 mythread1= new Mythread().new Mythread1();
         Mythread2 mythread2= new Mythread().new Mythread2();
-        MyCallable<Object> myCallable = new MyCallable<Object>();
+        //由Callable<Integer>创建一个FutureTask<Integer>对象：
+        MyCallable<Object> myCallable = new Mythread().new MyCallable<Object>();
+        //注释：FutureTask<Integer>是一个包装器，它通过接受Callable<Integer>来创建，它同时实现了Future和Runnable接口。
         FutureTask<Object> oneTask = new FutureTask<Object>(myCallable);
+        //由FutureTask<Integer>创建一个Thread对象：
         Thread myThread3 = new Thread(oneTask);
-        myThread3.start();
+        //使用ExecutorService、Callable、Future实现有返回结果的线程
+        Mythread4 myThread4 = new Mythread().new Mythread4();
+
         mythread1.start();
         mythread2.run();
-        System.out.println("主线程");
+        myThread3.start();
+        try {
+            myThread4.myRun();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
